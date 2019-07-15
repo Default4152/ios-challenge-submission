b0088d2c7a7f10d0358b3ba6943d100fcbd11f71
Conner Alegre <cga0@pm.me>
2019-07-15 15:56:16 -0600

    Initial commit with BaseTabBarController

---
+++ Tocca-Challenge/BaseTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseTabBarController.swift
+//  Tocca-Challenge
+//
+//  Created by Conner on 7/15/19.
+//  Copyright © 2019 Conner. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [
+            UIViewController(), UIViewController(), UIViewController()
+        ]
+    }
+}
