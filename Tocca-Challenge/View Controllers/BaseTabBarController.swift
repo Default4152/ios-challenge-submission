1778e2e032cf865d237c7e7927ae394b738fd238
Conner Alegre <cga0@pm.me>
2019-07-15 16:39:20 -0600

    Setup base view controllers for tab bar, add icons for tab bar

---
+++ Tocca-Challenge/View Controllers/BaseTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseTabBarController.swift
+//  Tocca-Challenge
+//
+//  Created by Conner on 7/15/19.
+//  Copyright © 2019 Conner. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.barTintColor = #colorLiteral(red: 0.9999126792, green: 1, blue: 0.9998814464, alpha: 1)
+        tabBar.tintColor = #colorLiteral(red: 0.1507650614, green: 0.3526057601, blue: 0.8449645638, alpha: 1)
+        
+        let contactsVC = ContactsViewController()
+        contactsVC.tabBarItem.image = UIImage(named: "contacts_icon")
+        contactsVC.tabBarItem.title = "Contacts"
+        
+        let cardVC = CardViewController()
+        cardVC.tabBarItem.image = UIImage(named: "my_card_icon")
+        cardVC.tabBarItem.title = "My Card"
+        
+        let settingsVC = SettingsViewController()
+        settingsVC.tabBarItem.image = UIImage(named: "settings_icon")
+        settingsVC.tabBarItem.title = "Settings"
+        
+        viewControllers = [cardVC, contactsVC, settingsVC]
+    }
+}
---
+++ Tocca-Challenge/View Controllers/ContactsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ContactsViewController.swift
+//  Tocca-Challenge
+//
+//  Created by Conner on 7/15/19.
+//  Copyright © 2019 Conner. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = #colorLiteral(red: 0.1507650614, green: 0.3526057601, blue: 0.8449645638, alpha: 1)
+    }
+}
